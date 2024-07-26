de52028bb8e982ce47fe0bed6229a0244e110681
INKmin9 <mkkim9901@naver.com>
2024-07-26 17:48:35 +0900

    🎨 improve: 디데이 계산 함수 수정

--- src/utils/DdayCalculator.jsx
+++ src/utils/DdayCalculator.jsx
@@ -1,9 +1,11 @@
-export default function DdayCalculator() {
+export default function DdayCalculator(createdAt) {
   var today = new Date();
-  var createdAt = new Date("2024-07-18"); // createdAT 받아와서 계산
+  var createdDate = new Date(createdAt); // createdAT 받아와서 계산
 
-  var timeDiff = today.getTime() - createdAt.getTime();
-  var dDay = Math.floor(timeDiff / (1000 * 60 * 60 * 24) + 1);
+  // 두 날짜의 차이 계산
+  var timeDiff = today.getTime() - createdDate.getTime();
+  // 날짜 차이를 일 단위로 변환
+  var dDay = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
 
   return dDay;
 }
