87d5965fd37c01e7c91d324c73c14628092d2891
INKmin9 <mkkim9901@naver.com>
2024-08-01 10:33:51 +0900

    ✨ feat: axios인스턴스 베이스 url 수정 - .env.local에 베이스url추가

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 //임의생성
 const instance = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 //요청 인터셉터를 추가하여 모든 요청에 토큰 포함
